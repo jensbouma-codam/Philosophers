efe85620ef69c8836e3777be206eefb563da9fba
Jens Bouma <account@jensbouma.nl>
2023-07-27 02:01:55 +0200

    🐛 fix(philo.c): fix typo in variable name 'time_to_die_mutex' in philo_free function 🐛 fix(philo.c): fix typo in variable name 'time_to_die_mutex' in philo_create function 🐛 fix(philo.c): fix typo in variable name 'time_to_die_mutex' in philo_proc function 🐛 fix(philo.c): fix typo in variable name 'time_to_die_mutex' in someone_died function 🐛 fix(philo.c): fix typo in variable name 'time_to_die_mutex' in simulation function 🐛 fix(philo.c): fix typo in variable name 'time_to_die_mutex' in simulation_run function 🐛 fix(philo.c): fix typo in variable name 'time_to_die_mutex' in everbody_has_eaten function 🐛 fix(philo.c): fix typo in variable name 'time_to_die_mutex' in processcount function 🐛 fix(philo.c): fix typo in variable name 'time_to_die_mutex' in simulation_monitor function ✨ feat(philo.c): add mutex destruction for time_to_die_mutex in philo_free function ✨ feat(philo.c): add mutex initialization for time_to_die_mutex in philo_create function ✨ feat(philo.c): add mutex initialization for time_to_die_mutex in philo_proc function ✨ feat(philo.c): add mutex initialization for time_to_die_mutex in simulation function ✨ feat(philo.c): add mutex initialization for time_to_die_mutex in simulation_run function ✨ feat(philo.c): add mutex initialization for time_to_die_mutex in everbody_has_eaten function ✨ feat(philo.c): add mutex initialization for time_to_die_mutex in processcount function ✨ feat(philo.c): add mutex initialization for time_to_die_mutex in simulation_monitor function

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,7 +6,7 @@
 /*   By: jensbouma <jensbouma@student.codam.nl>       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/24 23:34:38 by jensbouma     #+#    #+#                 */
-/*   Updated: 2023/07/26 19:10:45 by jbouma        ########   odam.nl         */
+/*   Updated: 2023/07/27 01:53:55 by jensbouma     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,42 +19,43 @@ int	philo_free(void *ptr)
 	p = (t_philo *)ptr;
 	if (pthread_detach(p->thread) != 0)
 		errorlog("Failed to detach thread");
+	if (pthread_mutex_destroy(&p->time_to_die_mutex) != 0)
+		errorlog("Failed to destroy n_to_die_mutex mutex");
 	free(p);
 	return (SUCCESS);
 }
 
-void	philo_think(t_philo *p, t_sim *s)
-{
-	if (p->state != THINKING)
-		msg_add(s, p->id, "is thinking", false);
-	p->state = THINKING;
-}
-
 void	philo_eat(t_philo *p, t_sim *s, t_fork *fork_l, t_fork *fork_r)
 {
-	if (p->id % 2 == 0)
-	{
-		usleep(1000);
-		pthread_mutex_lock(&fork_r->in_use_mutex);
-		pthread_mutex_lock(&fork_l->in_use_mutex);
-	}
-	else
-	{
-		pthread_mutex_lock(&fork_l->in_use_mutex);
-		pthread_mutex_lock(&fork_r->in_use_mutex);
-	}
+	pthread_mutex_lock(&fork_l->in_use_mutex);
 	msg_add(s, p->id, "has taken a fork", false);
+	pthread_mutex_lock(&fork_r->in_use_mutex);
 	msg_add(s, p->id, "has taken a fork", false);
-	p->state = EATING;
+	pthread_mutex_lock(&p->time_to_die_mutex);
+	p->time_to_die = timestamp(s) + s->time_to_die;
+	pthread_mutex_unlock(&p->time_to_die_mutex);
 	msg_add(s, p->id, "is eating", false);
-	pthread_mutex_lock(&p->n_to_die_mutex);
-	p->n_to_die = timestamp(s) + s->n_to_die;
-	pthread_mutex_unlock(&p->n_to_die_mutex);
-	spend_time(s, s->n_to_eat);
+	spend_time(s, s->time_to_eat);
 	p->x_eaten.set(&p->x_eaten, p->x_eaten.get(&p->x_eaten) + 1);
-	msg_add(s, p->id, "is sleeping", false);
 	pthread_mutex_unlock(&fork_r->in_use_mutex);
 	pthread_mutex_unlock(&fork_l->in_use_mutex);
+}
+
+void	wait_for_start(t_sim *s, t_philo *p)
+{
+	while (true)
+	{
+		if (s->end_sim.get(&s->end_sim))
+			return ;
+		else if (s->start_lock.get(&s->start_lock) == false)
+			break ;
+		usleep(100);
+	}
+	if (p->id % 2 == 1)
+	{
+		msg_add(s, p->id, "is thinking", false);
+		spend_time(s, s->time_to_eat / 2);
+	}
 }
 
 void	*philo_proc(void *ptr)
@@ -62,24 +63,25 @@ void	*philo_proc(void *ptr)
 	t_philo	*p;
 	t_sim	*s;
 	t_fork	*fork_r;
+	t_fork	*fork_l;
 
 	p = (t_philo *)ptr;
 	s = p->sim;
-	p->state = INIT;
 	if (p->id + 1 == s->count)
 		fork_r = (t_fork *)s->forks.get(&s->forks, 0);
 	else
 		fork_r = (t_fork *)s->forks.get(&s->forks, (p->id + 1));
+	fork_l = (t_fork *)s->forks.get(&s->forks, p->id);
 	p->running.set(&p->running, true);
-	pthread_mutex_lock(&s->start_lock_mutex);
-	pthread_mutex_unlock(&s->start_lock_mutex);
-	while (!s->has_eaten.get(&s->has_eaten) && !s->one_died.get(&s->one_died))
+	wait_for_start(s, p);
+	while (!s->end_sim.get(&s->end_sim))
 	{
-		if (((t_fork *)s->forks.get(&s->forks, p->id))->id == fork_r->id)
+		if (fork_l->id == fork_r->id)
 			continue ;
-		philo_think(p, s);
-		philo_eat(p, s, (t_fork *)s->forks.get(&s->forks, p->id), fork_r);
-		spend_time(s, s->n_to_sleep);
+		philo_eat(p, s, fork_l, fork_r);
+		msg_add(s, p->id, "is sleeping", false);
+		spend_time(s, s->time_to_sleep);
+		msg_add(s, p->id, "is thinking", false);
 	}
 	p->running.set(&p->running, false);
 	return (NULL);
@@ -92,12 +94,12 @@ int	philo_create(t_sim *s, int id)
 	p = ft_calloc(1, sizeof(t_philo));
 	if (!p)
 		return (errorlog("Malloc failed"), FAILURE);
-	if (pthread_mutex_init(&p->n_to_die_mutex, NULL) != 0)
-		return (errorlog("Failed to init mutex"), FAILURE);
+	if (pthread_mutex_init(&p->time_to_die_mutex, NULL) != 0)
+		return (free(p), errorlog("Failed to init mutex"), FAILURE);
 	if (s->philos.add(&s->philos, (void *)p) == FAILURE)
 		return (free(p), errorlog("Malloc failed"), FAILURE);
 	p->id = id;
-	p->n_to_die = s->n_to_die;
+	p->time_to_die = s->time_to_die;
 	p->sim = s;
 	if (!value_init(&p->x_eaten) || !value_init(&p->running))
 		return (errorlog("Failed to init value"), FAILURE);
--- philo/src/sim.c
+++ philo/src/sim.c
@@ -6,91 +6,95 @@
 /*   By: jensbouma <jensbouma@student.codam.nl>       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/25 18:04:15 by jensbouma     #+#    #+#                 */
-/*   Updated: 2023/07/26 19:29:23 by jbouma        ########   odam.nl         */
+/*   Updated: 2023/07/27 01:53:01 by jensbouma     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-bool	everbody_has_eaten(t_sim *sim)
+bool	everbody_has_eaten(t_sim *s)
 {
 	int			id;
 	t_philo		*philo;
 
 	id = 0;
-	if (sim->times_to_eat == -1)
+	if (s->times_to_eat == -1)
 		return (false);
-	while (id < sim->count)
+	while (id < s->count)
 	{
-		philo = (t_philo *)sim->philos.get(&sim->philos, id);
-		if (philo->x_eaten.get(&philo->x_eaten) < sim->times_to_eat)
+		philo = (t_philo *)s->philos.get(&s->philos, id);
+		if (philo->x_eaten.get(&philo->x_eaten) < s->times_to_eat)
 			return (false);
 		id++;
 	}
-	if (!sim->has_eaten.get(&sim->has_eaten))
-	{
-		if (DEBUG)
-			msg_add(sim, -1, "All philosophers have eaten enough", true);
-		sim->has_eaten.set(&sim->has_eaten, true);
-	}
+	if (DEBUG)
+		msg_add(s, -1, "All philosophers have eaten enough", true);
 	return (true);
 }
 
-bool	someone_died(t_sim *sim)
+bool	someone_died(t_sim *s)
 {
 	int			id;
 	t_philo		*philo;
 
 	id = 0;
-	while (id < sim->count)
+	while (id < s->count)
 	{
-		philo = (t_philo *)sim->philos.get(&sim->philos, id);
-		if (pthread_mutex_lock(&philo->n_to_die_mutex) != 0)
-			return (errorlog("Failed to lock mutex"), false);
-		if (philo->n_to_die < timestamp(sim)
+		philo = (t_philo *)s->philos.get(&s->philos, id);
+		pthread_mutex_lock(&philo->time_to_die_mutex);
+		if (philo->time_to_die < timestamp(s)
 			&& philo->running.get(&philo->running))
 		{
-			msg_add(sim, id, "died", true);
-			sim->one_died.set(&sim->one_died, true);
-			pthread_mutex_unlock(&philo->n_to_die_mutex);
+			msg_add(s, id, "died", true);
+			pthread_mutex_unlock(&philo->time_to_die_mutex);
 			return (true);
 		}
-		pthread_mutex_unlock(&philo->n_to_die_mutex);
+		pthread_mutex_unlock(&philo->time_to_die_mutex);
 		id++;
 	}
 	return (false);
 }
 
-int	processcount(t_sim *sim)
+int	processcount(t_sim *s)
 {
 	int		id;
 	int		count;
 
 	id = 0;
 	count = 0;
-	while (id < sim->count)
+	while (id < s->count)
 	{
-		if (((t_philo *)sim->philos.get(&sim->philos, id))->running
-			.get(&((t_philo *)sim->philos.get(&sim->philos, id))->running))
+		if (((t_philo *)s->philos.get(&s->philos, id))->running
+			.get(&((t_philo *)s->philos.get(&s->philos, id))->running))
 			count++;
 		id++;
 	}
 	return (count);
 }
 
-void	simulation_monitor(t_sim *s)
+void	simulation_run(t_sim *s)
 {
-	while (msg_print(s))
+	int	count;
+
+	count = 0;
+	timestamp(s);
+	s->start_lock.set(&s->start_lock, false);
+	while (true)
 	{
-		if (someone_died(s))
+		if (someone_died(s) || everbody_has_eaten(s))
 			break ;
-		if (everbody_has_eaten(s))
-			break ;
-		usleep(1000);
+		else if (count++ == 100)
+		{
+			msg_print(s);
+			count = 0;
+		}
+		else
+			usleep(100);
 	}
+	s->end_sim.set(&s->end_sim, true);
 	msg_print(s);
 	while (processcount(s) > 0)
-		usleep(10000);
+		usleep(1000);
 }
 
 int	simulation(t_sim *s)
@@ -100,22 +104,21 @@ int	simulation(t_sim *s)
 
 	id = 0;
 	v_init(&s->forks, sizeof(t_fork), fork_free, NULL);
+	value_init(&s->start_lock);
+	s->start_lock.set(&s->start_lock, true);
 	if (!fork_create(s))
 		return (s->forks.free(&s->forks), FAILURE);
 	while (id < s->count)
 		if (!philo_create(s, id++))
 			return (s->forks.free(&s->forks), FAILURE);
-	timestamp(s);
-	pthread_mutex_unlock(&s->start_lock_mutex);
-	simulation_monitor(s);
-	while (--id >= 0)
+	simulation_run(s);
+	while (id--)
 	{
 		p = (t_philo *)s->philos.get(&s->philos, id);
 		p->running.free(&p->running);
 		p->x_eaten.free(&p->x_eaten);
 	}
-	s->has_eaten.free(&s->has_eaten);
-	s->one_died.free(&s->one_died);
+	s->end_sim.free(&s->end_sim);
 	v_free(&s->forks);
 	if (pthread_mutex_destroy(&s->msg_mutex) != 0)
 		return (errorlog("Failed to destroy mutex"), FAILURE);
