e85f53af79b050278aeb6c0b91e1dde1bf172e88
Jens Bouma <account@jensbouma.nl>
2023-07-28 15:25:28 +0200

    🐛 fix(main.c): fix memory leak by freeing resources before returning EXIT_FAILURE ✨ feat(main.c): initialize and free the 'forks' vector in the simulation 🐛 fix(main.c): fix error handling when initializing program resources 🐛 fix(main.c): fix error handling when creating forks 🐛 fix(main.c): fix error handling when running simulation 🐛 fix(main.c): fix error handling when destroying mutexes 🐛 fix(msg.c): fix error handling when allocating memory for new message

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: jbouma <jbouma@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/12 15:23:32 by jbouma        #+#    #+#                 */
-/*   Updated: 2023/07/27 02:17:29 by jensbouma     ########   odam.nl         */
+/*   Updated: 2023/07/28 15:21:15 by jensbouma     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,23 +21,28 @@ static void	print_mem(void)
 int	main(int argc, char **argv)
 {
 	t_sim	*s;
+	int		exit;
 
 	if (DEBUG == 2)
 		atexit(print_mem);
+	exit = EXIT_FAILURE;
 	s = input(argc, argv, 0);
 	if (!s)
 		return (EXIT_FAILURE);
-	if (!v_init(&s->msg, sizeof(t_msg), msg_free, NULL)
-		|| !v_init(&s->philos, sizeof(t_philo), philo_free, NULL))
-		return (free(s), errorlog("Malloc failed"));
-	if (!value_init(&s->end_sim)
-		|| pthread_mutex_init(&s->msg_mutex, NULL) != 0
-		|| pthread_mutex_init(&s->time_mutex, NULL) != 0)
-		return (free(s), errorlog("Failed to init program"));
-	if (!simulation(s))
-		return (v_free(&s->philos), v_free(&s->msg), free(s), EXIT_FAILURE);
-	if (pthread_mutex_destroy(&s->msg_mutex) != 0
-		|| pthread_mutex_destroy(&s->time_mutex) != 0)
-		return (errorlog("Failed to destroy mutex"), FAILURE);
-	return (v_free(&s->philos), v_free(&s->msg), free(s), EXIT_SUCCESS);
+	if (pthread_mutex_init(&s->msg_mutex, NULL) != 0
+		|| pthread_mutex_init(&s->time_mutex, NULL) != 0
+		|| !v_init(&s->philos, sizeof(t_philo), philo_free, NULL)
+		|| !v_init(&s->msg, sizeof(t_msg), msg_free, NULL)
+		|| !v_init(&s->forks, sizeof(t_fork), fork_free, NULL)
+		|| !fork_create(s))
+		errorlog("Failed to init program");
+	else
+		exit = simulation(s);
+	v_free(&s->philos);
+	v_free(&s->forks);
+	v_free(&s->msg);
+	pthread_mutex_destroy(&s->time_mutex);
+	pthread_mutex_destroy(&s->msg_mutex);
+	free(s);
+	return (exit);
 }
